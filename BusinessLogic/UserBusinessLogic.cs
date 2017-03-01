3b985056143abb67640a1357fd75d864f3da8c8c
Antoine <afreeman2280@gmail.com>
2017-03-01 11:35:25 -0500

    Made changes to dataAccess,web .config, businesslogic

--- BusinessLogic/UserBusinessLogic.cs
+++ BusinessLogic/UserBusinessLogic.cs
@@ -12,5 +12,52 @@ namespace BusinessLogic
 {
     public class UserBusinessLogic
     {
+        static UserDataAccess _dal = new UserDataAccess();
+        static DataAccessUser daUser = new DataAccessUser();
+        public void createUserFromDatabase(UserBusinessLogic boCustomer)
+        {
+            DataAccessUser daCustomer = MapperBLL(boCustomer);
+            _dal.CreateDaUser(daCustomer);
+        }
+        static DataAccessUser MapperBLL(BusinessUserObjects boUser)
+        {
+
+            DataAccessUser daUser = new DataAccessUser();
+            daUser.UserName = boUser.UserName;
+            daUser.Password = boUser.Password;
+            daUser.FirstName = boUser.FirstName;
+            daUser.LastName = boUser.LastName;
+            daUser.Email = boUser.Email;
+            daUser.Birthdate = boUser.Birthdate;
+            daUser.City = boUser.City;
+            daUser.State = boUser.State;
+            daUser.StreetAddress = boUser.StreetAddress;
+            daUser.Zipcode = boUser.Zipcode;
+            daUser.SecurityQuestion = boUser.SecurityQuestion;
+            daUser.SecurityAnswer = boUser.SecurityAnswer;
+            daUser.Arrival = boUser.Arrival;
+            daUser.Depature = boUser.Depature;
+            return daUser;
+        }
+        static BusinessUserObjects MapperBLL(DataAccessUser daUser)
+        {
+
+            DataAccessUser daUser = new DataAccessUser();
+            daUser.UserName = boUser.UserName;
+            daUser.Password = boUser.Password;
+            daUser.FirstName = boUser.FirstName;
+            daUser.LastName = boUser.LastName;
+            daUser.Email = boUser.Email;
+            daUser.Birthdate = boUser.Birthdate;
+            daUser.City = boUser.City;
+            daUser.State = boUser.State;
+            daUser.StreetAddress = boUser.StreetAddress;
+            daUser.Zipcode = boUser.Zipcode;
+            daUser.SecurityQuestion = boUser.SecurityQuestion;
+            daUser.SecurityAnswer = boUser.SecurityAnswer;
+            daUser.Arrival = boUser.Arrival;
+            daUser.Depature = boUser.Depature;
+            return daUser;
+        }
     }
 }
--- DataAccess/UserDataAccess.cs
+++ DataAccess/UserDataAccess.cs
@@ -19,5 +19,47 @@ namespace DataAccess
         DataAccessUser user = new DataAccessUser();
           string _connection = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
           string _FileLocation = "C:/Users/Admin2/Desktop/important3.txt";
+        public void CreateDaUser(DataAccessUser customer)
+        {
+            SqlConnection lConnection = new SqlConnection(_connection);
+            SqlCommand cmd = new SqlCommand("SP_CreateNewCustomer", lConnection);
+            try
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                lConnection.Open();
+                cmd.Parameters.AddWithValue("@UserName", customer.UserName);
+                cmd.Parameters.AddWithValue("@Password", customer.Password);
+                cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", customer.LastName);
+                cmd.Parameters.AddWithValue("@Email", customer.Email);
+                cmd.Parameters.AddWithValue("@Birthdate", customer.Birthdate);
+                cmd.Parameters.AddWithValue("@City", customer.City);
+                cmd.Parameters.AddWithValue("@State", customer.State);
+                cmd.Parameters.AddWithValue("@Address", customer.StreetAddress);
+                cmd.Parameters.AddWithValue("@Zipcode", customer.Zipcode);
+                cmd.Parameters.AddWithValue("@Question", customer.SecurityQuestion);
+                cmd.Parameters.AddWithValue("@Answer", customer.SecurityAnswer);
+                cmd.Parameters.AddWithValue("@Arrival", customer.Arrival);
+                cmd.Parameters.AddWithValue("@Departure", customer.Depature);
+
+            }
+            catch (SqlException error)
+            {
+
+                string _ErrorLogFileLocation = @"C:\Users\admin2\Desktop\\ErrorLog.txt";
+                using (StreamWriter lwriter = new StreamWriter(_ErrorLogFileLocation, true))
+                {
+                    lwriter.WriteLine(error.Message);
+
+                }
+            }
+            finally
+            {
+                lConnection.Close();
+            }
+
+
+
+        }
     }
 }
